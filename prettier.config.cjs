b6aef179d41b1e7ee76b65f7431687467d8075b0
Alexis Guzman <codingcodax@gmail.com>
2023-11-19 19:52:47 -0600

    feat(prettier): create basic config

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+};
