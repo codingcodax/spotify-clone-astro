129823e9f367784c01979cf2af3fff676e257a9c
Alexis Guzman <codingcodax@gmail.com>
2023-11-19 19:54:51 -0600

    feat(prettier): add sort imports, prettier and tailwind type defs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,7 @@
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 module.exports = {
   arrowParens: 'always',
