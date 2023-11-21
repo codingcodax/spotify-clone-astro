34ac34ee68865ed4f76e07faa47b69bd9a758bba
Alexis Guzman <codingcodax@gmail.com>
2023-11-20 20:37:34 -0600

    feat(eslint): extend typescript plugin and add rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['plugin:astro/recommended'],
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
   overrides: [
     {
       files: ['*.astro'],
@@ -8,6 +11,30 @@ module.exports = {
       parserOptions: {
         parser: '@typescript-eslint/parser',
         extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+        ],
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          {
+            checksVoidReturn: {
+              // Allow `void` return type for JSX attributes such as `onClick` / `onSubmit`
+              attributes: false,
+            },
+          },
+        ],
       },
     },
   ],
